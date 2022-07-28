4700c6d2a1b088a7332093c5d16a028954a32a4b
Foysal Ahamed Badsha <you@example.com>
2022-07-28 14:06:27 +0600

    mongodb account change

--- index.js
+++ index.js
@@ -3,24 +3,31 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
+//Midddle Ware
 app.use(cors());
 app.use(express.json());
 
 //MongoDB Connect
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.juxo7.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lqv7isf.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('MongoDB connected')
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database Connected');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Webb School website!')
+    res.send('Webb School website !!!!!!!')
 })
 app.listen(port, () => {
     console.log(`Hello everyone ${port}`)
