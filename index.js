015062557d3d498eddc6118b0bb5420a84e37581
Foysal Ahamed Badsha <you@example.com>
2022-08-09 12:08:18 +0600

    install JWT

--- index.js
+++ index.js
@@ -12,58 +12,57 @@ app.use(express.json());
 //MongoDB Connect
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lqv7isf.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
-  try {
-    await client.connect();
-    const languageCollection = client.db("courses").collection("language");
-    const admissionCollection = client.db("courses").collection("admission");
-    const jobCollection = client.db("courses").collection("job");
-    const playCollection = client.db("Videos").collection("courseplaylist");
+    try {
+        await client.connect();
+        const languageCollection = client.db("courses").collection("language");
+        const admissionCollection = client.db("courses").collection("admission");
+        const jobCollection = client.db("courses").collection("job");
+        const playCollection = client.db("Videos").collection("courseplaylist");
 
-    // courses -Start
-    app.get("/language", async (req, res) => {
-      const query = {};
-      const cursor = languageCollection.find(query);
-      const courses = await cursor.toArray();
-      res.send(courses);
-    });
-    app.get("/admission", async (req, res) => {
-      const query = {};
-      const cursor = admissionCollection.find(query);
-      const courses = await cursor.toArray();
-      res.send(courses);
-    });
-    app.get("/job", async (req, res) => {
-      const query = {};
-      const cursor = jobCollection.find(query);
-      const courses = await cursor.toArray();
-      res.send(courses);
-    });
-    // courses -End
-    app.get("/videos", async (req, res) => {
-      const query = {};
-      const cursor = playCollection.find(query);
-      const videos = await cursor.toArray();
-      res.send(videos);
-    });
-  }
-  finally{
+        // courses -Start
+        app.get("/language", async (req, res) => {
+            const query = {};
+            const cursor = languageCollection.find(query);
+            const courses = await cursor.toArray();
+            res.send(courses);
+        });
+        app.get("/admission", async (req, res) => {
+            const query = {};
+            const cursor = admissionCollection.find(query);
+            const courses = await cursor.toArray();
+            res.send(courses);
+        });
+        app.get("/job", async (req, res) => {
+            const query = {};
+            const cursor = jobCollection.find(query);
+            const courses = await cursor.toArray();
+            res.send(courses);
+        });
+        // courses -End
+        app.get("/videos", async (req, res) => {
+            const query = {};
+            const cursor = playCollection.find(query);
+            const videos = await cursor.toArray();
+            res.send(videos);
+        });
+    }
+    finally {
 
-  }
+    }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
